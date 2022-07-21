1efc1e53abfdbaea71492c8a73c46e801fe96484
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-07-21 16:09:36 +0300

    Refactor variable names

--- main.py
+++ main.py
@@ -14,21 +14,21 @@ def get_service_response(url: str) -> dict:
 
 
 def get_comics_url(service_response: dict) -> str:
-    comics_url = service_response['img']
-    return comics_url
+    comic_url = service_response['img']
+    return comic_url
 
 
 def get_comics_extension(service_response: dict) -> str:
-    comics_url = service_response['img']
-    comics_path = urlparse(url=comics_url).path
+    comic_url = service_response['img']
+    comic_path = urlparse(url=comic_url).path
 
-    return os.path.splitext(comics_path)[-1]
+    return os.path.splitext(comic_path)[-1]
 
 
 def get_comics_filename(service_response: dict) -> str:
-    comics_name = service_response['title']
-    comics_extension = get_comics_extension(service_response=service_response)
-    filename = f'{comics_name}.{comics_extension}'
+    comic_name = service_response['title']
+    comic_extension = get_comics_extension(service_response=service_response)
+    filename = f'{comic_name}{comic_extension}'
 
     return filename
 
@@ -39,17 +39,17 @@ def download_comics() -> None:
 
     url = 'https://xkcd.com/353/info.0.json'
     service_response = get_service_response(url=url)
-    comics_url = get_comics_url(service_response=service_response)
+    comic_url = get_comics_url(service_response=service_response)
 
-    response = requests.get(url=comics_url)
+    response = requests.get(url=comic_url)
     response.raise_for_status()
 
-    comics = response.content
-    comics_name = get_comics_filename(service_response=service_response)
-    comics_path = os.path.join(download_folder, comics_name)
+    comic = response.content
+    comic_name = get_comics_filename(service_response=service_response)
+    comic_path = os.path.join(download_folder, comic_name)
 
-    with open(comics_path, 'wb') as comics_file:
-        comics_file.write(comics)
+    with open(comic_path, 'wb') as comics_file:
+        comics_file.write(comic)
 
 
 if __name__ == '__main__':
